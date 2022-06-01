9e2d7348f21d7de661edae690b0aef1029ae989f
Chad Jessup <chad.jessup@gmail.com>
2022-06-01 00:47:52 -0700

    hooking up identityserver

--- Sphere.Auth.API/Config.cs
+++ Sphere.Auth.API/Config.cs
@@ -1,4 +1,9 @@
-﻿using Duende.IdentityServer.Models;
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
+
+using IdentityModel;
+
+using Sphere.Shared;
 
 namespace Sphere.Auth.API;
 
@@ -9,6 +14,15 @@ public static class Config
         {
             new IdentityResources.OpenId(),
             new IdentityResources.Profile(),
+            new IdentityResource
+            {
+                Name = "verification",
+                UserClaims = new List<string>
+                {
+                    JwtClaimTypes.Email,
+                    JwtClaimTypes.EmailVerified,
+                }
+            }
         };
 
     public static IEnumerable<ApiScope> ApiScopes =>
@@ -32,6 +46,29 @@ public static class Config
                 {
                     "api1",
                 }
-            }
+            },
+            new Client
+            {
+                ClientId = "web",
+                ClientSecrets =
+                {
+                   new Secret("secret".Sha256()),
+                },
+                AllowedGrantTypes = GrantTypes.Code,
+                // where to redirect to after login
+                RedirectUris = { Services.SphereWebTestApp.Combine("signin-oidc") },
+
+                AllowOfflineAccess = true,
+
+                // where to redirect to after logout
+                PostLogoutRedirectUris = { Services.SphereWebTestApp.Combine("signout-callback-oidc") },
+
+                AllowedScopes = new List<string>
+                {
+                    IdentityServerConstants.StandardScopes.OpenId,
+                    IdentityServerConstants.StandardScopes.Profile,
+                    "api1",
+                }
+            },
         };
 }
